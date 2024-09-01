f7ebd393fef3beada02c973b721ebaa5af6afaaa
Mohamed Abdallah <128813258+Mohamedkskdhkshx@users.noreply.github.com>
2024-09-01 22:35:17 +0300

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -9,3 +9,21 @@ $( function() {
   } );
 
 
+let iPlay = document.getElementById("i-play")
+let iPause = document.getElementById("i-pause")
+let video = document.getElementById("video")
+let overlay = document.getElementsByClassName("overlay")
+
+
+iPlay.addEventListener("click",function(){
+   if(video.paused){
+      video.play()
+     
+   }else{
+      video.pause();
+    
+   }
+
+})
+
+
