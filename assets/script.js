1308b58a1bfd7ae964c39a8fb9b18ba882b98afa
Mohamed Abdallah <128813258+Mohamedkskdhkshx@users.noreply.github.com>
2024-09-01 22:25:01 +0300

    Add files via upload

---
+++ assets/script.js
@@ -0,0 +1,27 @@
+$( function() {
+    $("#Check-In" ).datepicker();
+    $("#Check-out" ).datepicker();
+    $('.slider').slick({
+       dots:true,
+       
+    });
+
+  } );
+
+
+let iPlay = document.getElementById("i-play")
+let iPause = document.getElementById("i-pause")
+let video = document.getElementById("video")
+let overlay = document.getElementsByClassName("overlay")
+
+
+iPlay.addEventListener("click",function(){
+   if(video.paused){
+      video.play()
+     
+   }else{
+      video.pause();
+    
+   }
+
+})
